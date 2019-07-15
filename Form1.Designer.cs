e2d885e804f78c9af18edde63d5dd64c085ff6b3
Laggay-Arthur <39773470+Laggay-Arthur@users.noreply.github.com>
2019-07-15 10:43:29 +0300

    start

---
+++ Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace pixel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox0y = new System.Windows.Forms.TextBox();
+            this.textBox0x = new System.Windows.Forms.TextBox();
+            this.textBox1y = new System.Windows.Forms.TextBox();
+            this.textBox1x = new System.Windows.Forms.TextBox();
+            this.textBox2y = new System.Windows.Forms.TextBox();
+            this.textBox2x = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(379, 257);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(220, 275);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(52, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Точка";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox0y
+            // 
+            this.textBox0y.Location = new System.Drawing.Point(46, 301);
+            this.textBox0y.Name = "textBox0y";
+            this.textBox0y.Size = new System.Drawing.Size(48, 20);
+            this.textBox0y.TabIndex = 2;
+            this.textBox0y.Text = "10";
+            // 
+            // textBox0x
+            // 
+            this.textBox0x.Location = new System.Drawing.Point(46, 275);
+            this.textBox0x.Name = "textBox0x";
+            this.textBox0x.Size = new System.Drawing.Size(48, 20);
+            this.textBox0x.TabIndex = 3;
+            this.textBox0x.Text = "10";
+            // 
+            // textBox1y
+            // 
+            this.textBox1y.Location = new System.Drawing.Point(100, 300);
+            this.textBox1y.Name = "textBox1y";
+            this.textBox1y.Size = new System.Drawing.Size(54, 20);
+            this.textBox1y.TabIndex = 4;
+            this.textBox1y.Text = "200";
+            // 
+            // textBox1x
+            // 
+            this.textBox1x.Location = new System.Drawing.Point(100, 275);
+            this.textBox1x.Name = "textBox1x";
+            this.textBox1x.Size = new System.Drawing.Size(54, 20);
+            this.textBox1x.TabIndex = 5;
+            this.textBox1x.Text = "100";
+            // 
+            // textBox2y
+            // 
+            this.textBox2y.Location = new System.Drawing.Point(160, 300);
+            this.textBox2y.Name = "textBox2y";
+            this.textBox2y.Size = new System.Drawing.Size(54, 20);
+            this.textBox2y.TabIndex = 6;
+            this.textBox2y.Text = "150";
+            // 
+            // textBox2x
+            // 
+            this.textBox2x.Location = new System.Drawing.Point(160, 274);
+            this.textBox2x.Name = "textBox2x";
+            this.textBox2x.Size = new System.Drawing.Size(54, 20);
+            this.textBox2x.TabIndex = 7;
+            this.textBox2x.Text = "30";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(278, 275);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(52, 45);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Линия";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(336, 275);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(55, 45);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Треуг.";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 282);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(12, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "x";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 303);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "y";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(403, 326);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2x);
+            this.Controls.Add(this.textBox2y);
+            this.Controls.Add(this.textBox1x);
+            this.Controls.Add(this.textBox1y);
+            this.Controls.Add(this.textBox0x);
+            this.Controls.Add(this.textBox0y);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Базовая графика";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox0y;
+        private System.Windows.Forms.TextBox textBox0x;
+        private System.Windows.Forms.TextBox textBox1y;
+        private System.Windows.Forms.TextBox textBox1x;
+        private System.Windows.Forms.TextBox textBox2y;
+        private System.Windows.Forms.TextBox textBox2x;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pixel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox0x.TextChanged += button3_Click;
+            textBox0y.TextChanged += button3_Click;
+            textBox1x.TextChanged += button3_Click;
+            textBox1y.TextChanged += button3_Click;
+            textBox2x.TextChanged += button3_Click;
+            textBox2y.TextChanged += button3_Click;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x0 = Convert.ToInt32(textBox0x.Text);
+            int x1 = Convert.ToInt32(textBox1x.Text);
+            int x2 = Convert.ToInt32(textBox2x.Text);
+            int y0 = Convert.ToInt32(textBox0y.Text);
+            int y1 = Convert.ToInt32(textBox1y.Text);
+            int y2 = Convert.ToInt32(textBox2y.Text);
+
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            using (Graphics gr = Graphics.FromImage(bmp))
+            {
+                gr.FillRectangle(new SolidBrush(Color.Green), new Rectangle(x0, y0, 10, 10));
+            }
+            pictureBox1.Image = bmp;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int x0 = Convert.ToInt32(textBox0x.Text);
+            int x1 = Convert.ToInt32(textBox1x.Text);
+            int x2 = Convert.ToInt32(textBox2x.Text);
+            int y0 = Convert.ToInt32(textBox0y.Text);
+            int y1 = Convert.ToInt32(textBox1y.Text);
+            int y2 = Convert.ToInt32(textBox2y.Text);
+            // Массив точек треугольника.
+            Point[] points = new Point[3];
+            points[0].X = x0; points[0].Y = y0;
+            points[1].X = x1; points[1].Y = y1;
+            points[2].X = x2; points[2].Y = y2;
+
+            // Изображение, которое будем вставлять в PictureBox.
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            using (Graphics grfx = Graphics.FromImage(bmp))
+            {
+                // Рисуем.
+                grfx.DrawPolygon(Pens.Black, points);
+            }
+
+            // Устанавливаем изображение.
+            pictureBox1.Image = bmp;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int x0 = Convert.ToInt32(textBox0x.Text);
+            int x1 = Convert.ToInt32(textBox1x.Text);
+            int x2 = Convert.ToInt32(textBox2x.Text);
+            int y0 = Convert.ToInt32(textBox0y.Text);
+            int y1 = Convert.ToInt32(textBox1y.Text);
+            int y2 = Convert.ToInt32(textBox2y.Text);
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.DrawLine(new Pen(Brushes.Red, 4), new Point(x0, y0), new Point(x1, y1));
+            }
+            pictureBox1.Image = bmp;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new PictureBox().Image;
+        }
+    }
+}
